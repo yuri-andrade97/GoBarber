7dbfe0014b9d5c650b3f4553394ad4f4add31cfb
yuri.andrade <yuri.andarde@dbserver.com.br>
2021-11-14 11:24:05 -0300

    first commit

---
+++ src/models/Appointment.ts
@@ -0,0 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+
+class Appointment {
+  id: string;
+
+  provider: string;
+
+  date: Date;
+
+  constructor(provider: string, date: Date) {
+    this.id = uuidv4();
+    this.provider = provider;
+    this.date = date;
+  }
+}
+
+export default Appointment;
